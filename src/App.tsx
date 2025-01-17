d6616942f580c833d696c462f7d378cac58c4e25
Yari Torres Nicola <ybtorres9@gmail.com>
2025-01-16 19:17:06 -0500

    finish merge

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,10 @@ import Layout from "./pages/Layout";
 import Feed from "./components/custom-ui/Feed";
 import Login from "./pages/Login";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-<<<<<<< HEAD
-import NewPost from "./pages/NewPost";
-=======
 import HelpPage from "./pages/HelpPage";
 import PostForm from "./pages/PostForm";
 import UserPosts from "./pages/UserPosts";
 import SignUp from "./pages/SignUp";
->>>>>>> main
 
 const queryClient = new QueryClient();
 
@@ -24,18 +20,11 @@ function App() {
           <Routes>
             <Route path='login' element={<Login />} />
             <Route path='signup' element={<SignUp />} />
-
             <Route path='/' element={<Layout />}>
               <Route index element={<Feed />} />
-<<<<<<< HEAD
-              <Route path='login' element={<Login />} />
-              <Route path='submit' element={<NewPost />} />
-              {/* <Route path='nameOfPage' element={<CustomPage />} /> */}
-=======
               <Route path='/createPost' element={<PostForm />} />
               <Route path='/ask4help' element={<HelpPage />} />
               <Route path='/myPosts' element={<UserPosts />} />
->>>>>>> main
             </Route>
           </Routes>
         </Router>
