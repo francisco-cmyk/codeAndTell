e5087f2ca17c2fc6077e5e5f59e249d8decd2110
Francisco <fveranicola@gmail.com>
2025-03-19 14:38:43 -0400

    idk i forgot

--- src/components/custom-ui/Comment.tsx
+++ src/components/custom-ui/Comment.tsx
@@ -1,14 +1,16 @@
-import { createAcronym, showToast } from "../../lib/utils";
+import getMergeState, { createAcronym, showToast } from "../../lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui-lib/Avatar";
 import { htmlParser } from "../../lib/parser";
 import { Separator } from "../ui-lib/Separator";
 import { Button } from "../ui-lib/Button";
 import { MessageCircle, ThumbsUpIcon } from "lucide-react";
 import usePostLike from "../../hooks/usePostLike";
-import { forwardRef, useState } from "react";
+import { useState } from "react";
 import TiptapEditor from "./TipTapEditor";
 import usePostComment from "../../hooks/usePostComment";
 import { useQueryClient } from "@tanstack/react-query";
+
+const MAX_VISIBLE_REPLIES = 2;
 
 type Comment = {
   id: number;
@@ -31,163 +33,186 @@ type CommentProps = {
   postID: string;
   userID: string;
   querykey: string[]; // necessary for optimistic updates
-  isPanel?: boolean;
+  maxDepth?: number;
   commentsRef?: React.MutableRefObject<{
     [key: number]: HTMLDivElement | null;
   }>;
   onReply?: (id: number, name: string) => void;
 };
 
-const Comment = forwardRef<HTMLDivElement, CommentProps>(
-  ({ comment, userID, querykey, postID, isPanel, commentsRef, onReply }, _) => {
-    const queryClient = useQueryClient();
-    const [showEditor, setShowEditor] = useState<boolean>(false);
-    const [commentText, setCommentText] = useState<string>("");
+type State = {
+  showEditor: boolean;
+  commentText: string;
+  expanded: boolean;
+  visReplyCount: number;
+};
 
-    const { mutate: likeComment } = usePostLike();
-    const { mutate: postComment } = usePostComment();
+const initialState: State = {
+  showEditor: false,
+  commentText: "",
+  expanded: false,
+  visReplyCount: 2,
+};
 
-    const _isPanel = isPanel ?? false;
+export default function Comment({
+  comment,
+  userID,
+  querykey,
+  postID,
+  commentsRef,
+  maxDepth,
+  onReply,
+}: CommentProps) {
+  const queryClient = useQueryClient();
 
-    function setRef(node: HTMLDivElement | null) {
-      if (commentsRef) {
-        commentsRef.current[comment.id] = node;
-      }
-    }
+  const [state, setState] = useState(initialState);
+  const mergeState = getMergeState(setState);
 
-    function handleCommentLike() {
-      if (!comment || !userID) return;
+  const { mutate: likeComment } = usePostLike();
+  const { mutate: postComment, isPaused: isPendingPostComment } =
+    usePostComment();
 
-      likeComment({
-        commentID: comment.id,
-        userID,
-        key: querykey,
-        hasLiked: comment.userHasLiked,
+  const maxDepth_ = maxDepth ?? 2;
+
+  // Dynamically determine visible replies
+  const visibleReplies = comment.replies?.slice(0, state.visReplyCount);
+
+  // Check if there are more replies left to load
+  const hasMoreReplies =
+    comment.replies && state.visReplyCount < comment.replies.length;
+  console.log(hasMoreReplies);
+
+  function handleCommentLike() {
+    if (!comment || !userID) return;
+
+    likeComment({
+      commentID: comment.id,
+      userID,
+      key: querykey,
+      hasLiked: comment.userHasLiked,
+    });
+  }
+
+  function handleReply() {
+    mergeState({ showEditor: !state.showEditor });
+  }
+
+  function handleSubmitComment() {
+    if (!state.commentText) {
+      showToast({
+        type: "warning",
+        message: "please type a comment",
       });
+      return;
     }
-
-    function handleReply() {
-      if (!_isPanel) {
-        setShowEditor(!showEditor);
-      } else {
-        if (onReply) {
-          onReply(comment.id, comment.profile.name);
-        }
+    postComment(
+      {
+        userID,
+        postID,
+        content: state.commentText,
+        key: querykey,
+        parentCommentID: comment.id,
+      },
+      {
+        onSuccess: () => {
+          mergeState({ showEditor: false });
+          queryClient.invalidateQueries({
+            queryKey: querykey,
+          });
+        },
       }
-    }
-
-    function handleSubmitComment() {
-      if (!commentText) {
-        showToast({
-          type: "warning",
-          message: "please type a comment",
-        });
-        return;
-      }
-
-      if (!_isPanel) {
-        postComment(
-          {
-            userID,
-            postID,
-            content: commentText,
-            key: querykey,
-            parentCommentID: comment.id,
-          },
-          {
-            onSuccess: () => {
-              setShowEditor(false);
-              queryClient.invalidateQueries({
-                queryKey: querykey,
-              });
-            },
-          }
-        );
-      }
-    }
-
-    return (
-      <div
-        key={comment.id}
-        ref={setRef}
-        className={`${
-          comment.parentCommentID ? "ml-1" : "ml-0"
-        } flex flex-col text-sm min-h-fit`}
-      >
-        <div className='w-full flex mt-3'>
-          <Avatar className='h-5 w-5 mr-2'>
-            <AvatarImage src={comment.profile.avatarURL} alt='@profilePic' />
-            <AvatarFallback>
-              {createAcronym(comment.profile.name)}
-            </AvatarFallback>
-          </Avatar>
-
-          <p className='text-xs mb-2'>{comment.profile.name}</p>
-        </div>
-        <div className='pl-4 pb-2 mt-2'>{htmlParser(comment.content)}</div>
-        <div className='w-full flex justify-between items-center pl-2'>
-          <div className='flex items-center'>
-            <Button
-              variant='ghost'
-              className={`w-7 h-7 mr-0.5`}
-              onClick={handleCommentLike}
-            >
-              <ThumbsUpIcon
-                className={`${
-                  comment.userHasLiked ? "text-blue-600 dark:text-blue-400" : ""
-                }`}
-              />
-            </Button>
-
-            <p className='mr-2 font-semibold text-accent-foreground'>
-              {comment.likeCount}
-            </p>
-
-            <Button
-              variant='ghost'
-              className={`w-7 h-7 ${
-                showEditor ? "bg-zinc-300 dark:bg-zinc-700" : ""
-              }`}
-              onClick={handleReply}
-            >
-              <MessageCircle />
-            </Button>
-          </div>
-
-          <p className='text-xs'>{comment.createdAt}</p>
-        </div>
-        {!_isPanel && showEditor && (
-          <div className='px-4 mt-1'>
-            <TiptapEditor
-              value={commentText}
-              variant='small'
-              onChange={(value) => setCommentText(value)}
-              onSubmit={handleSubmitComment}
-            />
-          </div>
-        )}
-        {comment.replies && comment.replies.length > 0 && (
-          <div className='ml-2 border-l-[0.3px] pl-2'>
-            {comment.replies.map((reply) => (
-              <Comment
-                commentsRef={commentsRef}
-                key={reply.id}
-                comment={reply}
-                isPanel={_isPanel}
-                postID={postID}
-                querykey={querykey}
-                userID={userID}
-                onReply={onReply}
-              />
-            ))}
-          </div>
-        )}
-        <Separator
-          className={`${comment.parentCommentID ? "hidden" : "block"}  mt-2 `}
-        />
-      </div>
     );
   }
-);
 
-export default Comment;
+  return (
+    <div
+      key={comment.id}
+      className={`${
+        comment.parentCommentID ? "ml-1" : "ml-0"
+      } flex flex-col text-sm min-h-fit`}
+    >
+      <div className='w-full flex mt-3'>
+        <Avatar className='h-5 w-5 mr-2'>
+          <AvatarImage src={comment.profile.avatarURL} alt='@profilePic' />
+          <AvatarFallback>{createAcronym(comment.profile.name)}</AvatarFallback>
+        </Avatar>
+
+        <p className='text-xs mb-2'>{comment.profile.name}</p>
+      </div>
+      <div className='pl-4 pb-2 mt-2'>{htmlParser(comment.content)}</div>
+      <div className='w-full flex justify-between items-center pl-2'>
+        <div className='flex items-center'>
+          <Button
+            variant='ghost'
+            className={`w-7 h-7 mr-0.5`}
+            onClick={handleCommentLike}
+          >
+            <ThumbsUpIcon
+              className={`${
+                comment.userHasLiked ? "text-blue-600 dark:text-blue-400" : ""
+              }`}
+            />
+          </Button>
+
+          <p className='mr-2 font-semibold text-accent-foreground'>
+            {comment.likeCount}
+          </p>
+
+          <Button
+            variant='ghost'
+            className={`w-7 h-7 ${
+              state.showEditor ? "bg-zinc-300 dark:bg-zinc-700" : ""
+            }`}
+            onClick={handleReply}
+          >
+            <MessageCircle />
+          </Button>
+        </div>
+
+        <p className='text-xs'>{comment.createdAt}</p>
+      </div>
+      {state.showEditor && (
+        <div className='px-4 mt-1'>
+          <TiptapEditor
+            value={state.commentText}
+            variant='small'
+            isLoading={isPendingPostComment}
+            onChange={(value) => mergeState({ commentText: value })}
+            onSubmit={handleSubmitComment}
+          />
+        </div>
+      )}
+      {comment.replies && comment.replies.length > 0 && maxDepth_ > 0 && (
+        <div className='ml-2 border-l-[0.3px] pl-2'>
+          {visibleReplies?.map((reply) => (
+            <Comment
+              commentsRef={commentsRef}
+              key={reply.id}
+              comment={reply}
+              postID={postID}
+              querykey={querykey}
+              userID={userID}
+              maxDepth={maxDepth_ - 1}
+              onReply={onReply}
+            />
+          ))}
+          {/* Show More / Show Less Button */}
+          {hasMoreReplies && (
+            <Button
+              variant='ghost'
+              onClick={() => mergeState({ expanded: !state.expanded })}
+              className='text-blue-5001 p-1 text-xs'
+            >
+              {state.expanded
+                ? "Show Less Replies"
+                : `View More (${state.visReplyCount - comment.replies.length})`}
+            </Button>
+          )}
+        </div>
+      )}
+      <Separator
+        className={`${comment.parentCommentID ? "hidden" : "block"}  mt-2 `}
+      />
+    </div>
+  );
+}
--- src/hooks/usePostComment.ts
+++ src/hooks/usePostComment.ts
@@ -1,32 +1,67 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "../config/supabaseConfig";
 import { showToast } from "../lib/utils";
+import { PostType } from "../lib/types";
 
 type Params = {
   postID: string;
   userID: string;
   content: string;
   parentCommentID?: number;
-  key: string[];
+  key?: string[];
 };
 
 export default function usePostComment() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: ["commment"],
     mutationFn: async (params: Params) => {
-      const { error } = await supabase.from("comments").insert([
-        {
-          post_id: params.postID,
-          user_id: params.userID,
-          content: params.content,
-          parent_comment_id: params.parentCommentID ?? null,
-        },
-      ]);
+      const { data, error } = await supabase
+        .from("comments")
+        .insert([
+          {
+            post_id: params.postID,
+            user_id: params.userID,
+            content: params.content,
+            parent_comment_id: params.parentCommentID ?? null,
+          },
+        ])
+        .select("*")
+        .single();
       if (error) {
         throw new Error(error.message);
       }
+      return data;
     },
-    onError: (error) => {
+    onMutate: async ({ postID, userID, content, parentCommentID }) => {
+      await queryClient.cancelQueries({ queryKey: ["user-posts-by-id"] });
+      const previousData = queryClient.getQueryData(["user-posts-by-id"]);
+
+      const tempID = Date.now() - 1;
+      const newComment = {
+        id: tempID,
+        userID,
+        content,
+        postID,
+        parentCommentID,
+      };
+
+      queryClient.setQueryData(["user-posts-by-id"], (oldData: PostType) => {
+        if (!oldData || !oldData.comments) return oldData || { comments: [] };
+
+        return {
+          ...oldData,
+          comments: [...oldData.comments, newComment],
+        };
+      });
+
+      return { previousData, tempID };
+    },
+    onError: (error, _, context) => {
+      if (context?.previousData) {
+        queryClient.setQueryData(["user-posts-by-id"], context.previousData);
+      }
       if (error) {
         showToast({
           type: "error",
@@ -35,15 +70,25 @@ export default function usePostComment() {
         });
       }
     },
-    // onSuccess: (_, variables) => {
-    //   queryClient.invalidateQueries({
-    //     queryKey: [variables.key],
-    //   });
-    // },
-    // onSettled: (_, _1, variables) => {
-    //   queryClient.invalidateQueries({
-    //     queryKey: [variables.key],
-    //   });
-    // },
+    onSuccess: async (data, _, context) => {
+      if (!data.id) return;
+
+      const { tempID } = context;
+
+      queryClient.setQueryData(["user-posts-by-id"], (oldData: PostType) => {
+        if (!oldData || !oldData.comments) return oldData;
+
+        return {
+          ...oldData,
+          comments: oldData.comments.map((comment) =>
+            comment.id === tempID ? { ...comment, id: data.id } : comment
+          ),
+        };
+      });
+
+      await queryClient.invalidateQueries({
+        queryKey: ["user-posts-by-id"],
+      });
+    },
   });
 }
